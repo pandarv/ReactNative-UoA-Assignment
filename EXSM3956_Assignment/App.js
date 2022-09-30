65e28d0d476e2d0c8f4381f5cc3df6228974e54b
arvpandit <pandit.arv@gmail.com>
2022-09-30 09:18:49 -0600

    created two buttons (validation and today) and setup in App.js.

--- EXSM3956_Assignment/App.js
+++ EXSM3956_Assignment/App.js
@@ -1,20 +1,66 @@
+import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-
+import { SafeAreaView, StyleSheet, Text, View, TextInput, Button } from "react-native";
 export default function App() {
-	return (
-		<View style={styles.container}>
-			<Text>Hello World!!!</Text>
-			<StatusBar style="auto" />
-		</View>
-	);
+    const [inputVal, onChangeInputVal] = React.useState("");
+    const [value, setValue] = React.useState("");
+    console.log(inputVal);
+    const onClickHandle = () => {
+        setValue(inputVal);
+        onChangeInputVal("");
+    };
+    const onClickValidate = () => {
+        console.log(inputVal);
+    };
+    return (
+        <SafeAreaView style={styles.container}>
+            <View style={styles.topContainer}>
+                <Text style={styles.textSize}>Enter Birthdate</Text>
+            </View>
+            <View style={styles.bottomContainer}>
+                <Text>{value}</Text>
+                <TextInput style={styles.input} value={inputVal} onChangeText={onChangeInputVal} placeholder="mm-dd-yyyy" />
+            </View>
+            <View style={styles.buttonSetting}>
+                <Button title="Validation" onPress={onClickValidate} />
+                <Button title="Today?" onPress={onClickHandle} />
+            </View>
+        </SafeAreaView>
+    );
 }
-
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "#fff",
-		alignItems: "center",
-		justifyContent: "center",
-	},
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 0,
+        backgroundColor: "rgb(200, 200, 255)",
+        padding: 10,
+        borderRadius: 8,
+    },
+    textSize: {
+        fontSize: 32,
+    },
+    topContainer: {
+        flex: 0.1,
+        // width: 500,
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 24,
+        // backgroundColor: "rgb(128, 191, 255)",
+    },
+    bottomContainer: {
+        // flex: 0.1,
+        width: "100%",
+        // backgroundColor: "rgb(226, 198, 236)",
+    },
+    buttonSetting: {
+        flexDirection: "row",
+        margin: 16,
+    },
 });
