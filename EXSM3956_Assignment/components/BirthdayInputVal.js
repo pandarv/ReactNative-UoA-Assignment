f62b7482294363cf8c913e406721798664529a68
arvpandit <pandit.arv@gmail.com>
2022-10-02 22:51:33 -0600

    Calculated Last Birthday And Age in years

--- EXSM3956_Assignment/components/BirthdayInputVal.js
+++ EXSM3956_Assignment/components/BirthdayInputVal.js
@@ -1,119 +1,157 @@
 import React, { useState } from "react";
 import { SafeAreaView, StyleSheet, Text, View, TextInput, Button } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import { stringifyValueWithProperty } from "react-native-web/dist/cjs/exports/StyleSheet/compiler";
+// import { stringifyValueWithProperty } from "react-native-web/dist/cjs/exports/StyleSheet/compiler";
 import BirthdayWish from "./BirthdayWish";
 
 export default function BirthdayInputVal({ navigation }) {
-	const [inputDateValue, onChangeInputDateVal] = React.useState("");
-	const [value, setValue] = React.useState("");
-	const [error, setError] = React.useState("");
+    const [inputDateValue, onChangeInputDateVal] = React.useState("");
+    const [value, setValue] = React.useState("");
+    const [error, setError] = React.useState("");
+    const [age, setAge] = React.useState("");
+    const [birthday, setBirthday] = React.useState("");
+    const [toggle, setToggle] = React.useState(false);
 
-	const onClickValidate = () => {
-		const newDate = dateDisplay(inputDateValue).toDateString();
-		if (newDate == "Invalid Date") {
-			setError("Invalid Date. Please Enter correct date.");
-			// }
-			// else if (new Date(newDate) > new Date()) {
-			// 	setError("You are not Born Yet");
-		} else {
-			setValue(newDate);
-		}
-		onChangeInputDateVal("");
-	};
-	const dateDisplay = (val) => {
-		console.log("Val: " + val);
-		const d = new Date(val);
-		const userTimezoneOffset = d.getTimezoneOffset() * 60000;
-		const newD = new Date(d.getTime() + userTimezoneOffset);
-		const now = new Date();
-		console.log(`Currect: ${now} Given: ${d} ModDate: ${newD}`);
-		// console.log(`${month} - ${date} - ${year}`);
-		return newD;
-	};
-	// dateDisplay(value);
-	console.log(value);
+    const onClickValidate = () => {
+        if (inputDateValue.length < 10) {
+            setError("Please fillout yyyy-mm-dd");
+        } else {
+            const newDate = dateDisplay(inputDateValue).toDateString();
+            const years = new Date().getFullYear() - new Date(newDate).getFullYear();
+            // console.log(newDate);
+            if (newDate == "Invalid Date") {
+                // setToggle(false);
+                setError("Invalid Date. Please Enter correct date.");
+            } else if (new Date(newDate) > new Date()) {
+                setError("You are not Born Yet");
+                // setToggle(false);
+                // } else if ((new Date(newDate).getMonth() && new Date(newDate).getDate()) > (new Date().getMonth() && new Date().getDate())) {
+                //     console.log("Here: " + years);
+                // } else if (new Date(newDate) <= new Date()) {
+                //     const calculatedDate = new Date() - new Date(newDate);
+                //     const months = new Date().getMonth() - new Date(newDate).getMonth();
+                //     const days = new Date(calculatedDate).getDate();
+                //     console.log("Hmm: " + years);
+                //     setAge(`Year: ${years}`);
+                //     setValue(newDate);
+            } else {
+                const year = new Date(newDate).getFullYear();
+                const month = (new Date(newDate).getMonth() + 1).toLocaleString();
+                const modMonth = month.length == 1 ? "0" + month : month;
+                const day = new Date(newDate).getDate().toLocaleString();
+                const modDay = day.length == 1 ? "0" + day : day;
+                const yearsOld = (Date.now() - Date.parse(newDate)) / (1000 * 60 * 60 * 24 * 365.25);
+                const getYear = new Date(newDate).getFullYear() + Math.floor(yearsOld);
+                setBirthday(
+                    dateDisplay(`${getYear}-${modMonth}-${modDay}`).toLocaleDateString("en-US", { year: "numeric", month: "long", day: "numeric" })
+                );
+                setAge(`Year: ${Math.floor(yearsOld)}`);
+                setValue(newDate);
+                setToggle((prev) => !prev);
+            }
+        }
+        onChangeInputDateVal("");
+    };
+    const dateDisplay = (val) => {
+        const d = new Date(val);
+        const userTimezoneOffset = d.getTimezoneOffset() * 60000;
+        const newD = new Date(d.getTime() + userTimezoneOffset);
+        const now = new Date();
+        // console.log(`Currect: ${now} Given: ${d} ModDate: ${newD}`);
+        // console.log(`${month} - ${date} - ${year}`);
+        return newD;
+    };
+    // dateDisplay(value);
+    // console.log(value);
 
-	console.log(new Date(value) > new Date());
-	// console.log(dateDisplay(value).toDateString());
-	// console.log(new Date(value) == new Date());
+    // console.log(new Date(value) > new Date());
+    // console.log(dateDisplay(value).toDateString());
+    // console.log(new Date(value) == new Date());
 
-	const dateFormater = (inputData) => {
-		setError("");
-		const onlyDigits = inputData.replace(/[^\d]/g, "");
-		if (onlyDigits.length < 5) {
-			return onlyDigits;
-		} else if (onlyDigits.length < 7) {
-			return `${onlyDigits.slice(0, 4)}-${onlyDigits.slice(4)}`;
-		} else {
-			return `${onlyDigits.slice(0, 4)}-${onlyDigits.slice(4, 6)}-${onlyDigits.slice(6, 8)}`;
-		}
-	};
+    const dateFormater = (inputData) => {
+        setError("");
+        if (!inputData) return inputData;
+        const onlyDigits = inputData.replace(/[^\d]/g, "");
+        if (onlyDigits.length < 5) {
+            return onlyDigits;
+        } else if (onlyDigits.length < 7) {
+            return `${onlyDigits.slice(0, 4)}-${onlyDigits.slice(4)}`;
+        } else {
+            return `${onlyDigits.slice(0, 4)}-${onlyDigits.slice(4, 6)}-${onlyDigits.slice(6, 8)}`;
+        }
+    };
 
-	return (
-		<View style={styles.container}>
-			<View style={styles.topContainer}>
-				<Text style={styles.textSize}>Birthdate</Text>
-			</View>
-			<View style={styles.bottomContainer}>
-				<Text>After Today's Button Click: {value}</Text>
-				<Text>On change Value: {inputDateValue}</Text>
-				<TextInput style={styles.input} value={inputDateValue} onChangeText={(newText) => onChangeInputDateVal(dateFormater(newText))} placeholder="yyyy-mm-dd" />
-			</View>
-			<View>
-				{/* <Text>{new Date(value) > Date.now() ? "You are not Born Yet" : ""}</Text> */}
-				<Text>{error && error}</Text>
-			</View>
-			<View style={styles.buttonSetting}>
-				<Button title="Validation" onPress={onClickValidate} />
-				<Button
-					title="Today?"
-					disabled={!error || inputDateValue.length ? false : true}
-					onPress={() => {
-						navigation.navigate("Wish", { birthdate: value });
-					}}
-				/>
-			</View>
-		</View>
-	);
+    return (
+        <View style={styles.container}>
+            <View style={styles.topContainer}>
+                <Text style={styles.textSize}>Birthdate</Text>
+            </View>
+            <View style={styles.bottomContainer}>
+                <Text>DOB: {value}</Text>
+                <Text>Age: {age}</Text>
+                <Text>Last Birthday: {birthday}</Text>
+                <Text>On change Value: {inputDateValue}</Text>
+                <TextInput
+                    style={styles.input}
+                    value={inputDateValue}
+                    onChangeText={(newText) => onChangeInputDateVal(dateFormater(newText))}
+                    placeholder="yyyy-mm-dd"
+                />
+            </View>
+            <View>
+                {/* <Text>{new Date(value) > Date.now() ? "You are not Born Yet" : ""}</Text> */}
+                <Text>{error && error}</Text>
+            </View>
+            <View style={styles.buttonSetting}>
+                <Button title="Validation" onPress={onClickValidate} />
+                <Button
+                    title="Today?"
+                    disabled={toggle ? true : false}
+                    onPress={() => {
+                        navigation.navigate("Wish", { birthdate: value });
+                    }}
+                />
+            </View>
+        </View>
+    );
 }
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		alignItems: "center",
-		justifyContent: "top",
-		padding: 10,
-		backgroundColor: "white",
-	},
-	input: {
-		height: 40,
-		margin: 12,
-		borderWidth: 1,
-		borderColor: "rgb(116, 116, 255)",
-		backgroundColor: "rgb(200, 200, 255)",
-		borderRadius: 8,
-		paddingVertical: 8,
-		paddingHorizontal: 12,
-	},
-	textSize: {
-		fontSize: 32,
-	},
-	topContainer: {
-		flex: 0.4,
-		justifyContent: "center",
-		alignItems: "center",
-		padding: 24,
-	},
-	bottomContainer: {
-		// flex: 0.1,
-		width: "100%",
-	},
-	buttonSetting: {
-		flexDirection: "row",
-		justifyContent: "space-between",
-		margin: 24,
-	},
-	marginButton: {
-		margin: 4,
-	},
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "top",
+        padding: 10,
+        backgroundColor: "white",
+    },
+    input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        borderColor: "rgb(116, 116, 255)",
+        backgroundColor: "rgb(200, 200, 255)",
+        borderRadius: 8,
+        paddingVertical: 8,
+        paddingHorizontal: 12,
+    },
+    textSize: {
+        fontSize: 32,
+    },
+    topContainer: {
+        flex: 0.4,
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 24,
+    },
+    bottomContainer: {
+        // flex: 0.1,
+        width: "100%",
+    },
+    buttonSetting: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        margin: 24,
+    },
+    marginButton: {
+        margin: 4,
+    },
 });
--- EXSM3956_Assignment/components/BirthdayWish.js
+++ EXSM3956_Assignment/components/BirthdayWish.js
@@ -1,26 +1,92 @@
+import { useState, useEffect } from "react";
 import { SafeAreaView, StyleSheet, Text, View, TextInput, Button } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import BirthdayInputVal from "./BirthdayInputVal";
 
-const BirthdayWish = ({ route }) => {
-	return (
-		<View style={styles.container}>
-			<Text>Your Birthdate is: {route.params.birthdate}</Text>
-			<Text style={styles.textSize}>Happy Birthday!!🥳! </Text>
-		</View>
-	);
+const BirthdayWish = ({ navigation, route }) => {
+    const [bDay, setBDay] = useState(null);
+    const { birthdate } = route.params;
+    useEffect(() => {
+        const year = new Date(birthdate).getFullYear();
+        const month = (new Date(birthdate).getMonth() + 1).toLocaleString();
+        const modMonth = month.length == 1 ? "0" + month : month;
+        const day = new Date(birthdate).getDate().toLocaleString();
+        const modDay = day.length == 1 ? "0" + day : day;
+        const yearsOld = (Date.now() - Date.parse(birthdate)) / (1000 * 60 * 60 * 24 * 365.25);
+        const getYear = new Date(birthdate).getFullYear() + Math.floor(yearsOld);
+        // const nextBD = (Date.now() - Date.parse(birthdate) + 1000 * 60 * 60 * 24 * 365.25) / (1000 * 60 * 60 * 24 * 365.25);
+        console.log(`Years OLD: ${Math.floor(yearsOld)}`);
+        console.log(`Years Get Year: ${modMonth}`);
+        console.log(`Years Get Year: ${modDay}`);
+        const d = new Date(`${getYear}-${modMonth}-${modDay}`);
+        const userTimezoneOffset = d.getTimezoneOffset() * 60000;
+        const getNewBD = new Date(d.getTime() + userTimezoneOffset);
+        console.log(`New BD ${getNewBD}`);
+        // console.log(`Years OLD: ${Math.floor(nextBD)}`);
+        // console.log(`Years OLD: ${new Date(nextBD)}`);
+        // console.log(daysLived);
+        if ((new Date().getMonth() && new Date().getDate()) == (new Date(birthdate).getMonth() && new Date(birthdate).getDate())) {
+            setBDay("Happy Birthday!!! 🥳");
+        }
+        // if ((new Date().getMonth() && new Date().getDate()) < (new Date(birthdate).getMonth() && new Date(birthdate).getDate())){
+        // 	const
+        // }
+    }, [birthdate]);
+    console.log(`From Month: ${new Date(birthdate)}`);
+    // console.log(`From Month: ${new Date(birthdate).getMonth()}`);
+    // console.log(`From Year: ${new Date(birthdate).getUTCFullYear()}`);
+    // console.log(`From Year: ${new Date(birthdate).getDate()}`);
+    // console.log(`From Today: ${new Date()}`);
+    // // console.log(`Mil Sec Now : ${Date.now()}`);
+    // console.log(`DOB in Mil Sec: ${Date.parse(birthdate)}`);
+    // console.log(`Age Days: ${(Date.now() - Date.parse(birthdate)) / (1000 * 60 * 60 * 24)}`);
+    // console.log(`Age remaining days: ${(Date.now() - Date.parse(birthdate)) / (1000 * 60 * 60 * 24) - 51 * 365.25}`);
+    // console.log(`Age Years: ${(Date.now() - Date.parse(birthdate)) / (1000 * 60 * 60 * 24 * 365.25)}`);
+    // console.log(`Age Years: ${41 * 365.25}`);
+    return (
+        <View style={styles.container}>
+            <View style={styles.topContainer}>
+                <Text>Your Birthdate is on: {birthdate}</Text>
+            </View>
+            <View style={styles.bottomContainer}>
+                <Text style={styles.textSize}>{bDay && bDay}</Text>
+            </View>
+            {/* <Button
+                title="Back"
+                // disabled={!error || inputDateValue.length ? false : true}
+                onPress={() => {
+                    navigation.navigate("Home");
+                }}
+            /> */}
+        </View>
+    );
 };
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "white",
-		alignItems: "center",
-		justifyContent: "center",
-		// backgroundColor: "rgb(55,55,55)",
-	},
-	textSize: {
-		fontSize: 24,
-	},
+    container: {
+        flex: 1,
+        backgroundColor: "white",
+        alignItems: "center",
+        justifyContent: "center",
+        // backgroundColor: "rgb(55,55,55)",
+    },
+    textSize: {
+        fontSize: 36,
+    },
+    topContainer: {
+        flex: 0.2,
+        width: " 100%",
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 24,
+        backgroundColor: "#d6cbc1",
+    },
+    bottomContainer: {
+        flex: 0.5,
+        width: "100%",
+        alignItems: "center",
+        justifyContent: "top",
+        backgroundColor: "#CDD6D0",
+    },
 });
 
 export default BirthdayWish;
